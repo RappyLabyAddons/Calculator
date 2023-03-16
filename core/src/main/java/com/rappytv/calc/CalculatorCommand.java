ae9de0e0936c78a801ac53aefef2695b42a336e4
RappyTV <rappytv@rappytv.com>
2023-03-16 16:01:04 +0100

    Added utility functions

--- core/src/main/java/com/rappytv/calc/CalculatorCommand.java
+++ core/src/main/java/com/rappytv/calc/CalculatorCommand.java
@@ -2,6 +2,10 @@ package com.rappytv.calc;
 
 import net.labymod.api.client.chat.command.Command;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 @ConfigName("settings")
 public class CalculatorCommand extends Command {
@@ -21,11 +25,26 @@ public class CalculatorCommand extends Command {
         }
         try {
             String problem = String.join(" ", arguments);
-            addon.displayMessage(CalculatorAddon.prefix + problem + " = " + "<solution>");
-        } catch (Exception e) {
+            addon.displayMessage(CalculatorAddon.prefix + problem + " = " + formatNumber(solveProblem(problem)));
+        } catch (NumberFormatException e) {
             addon.displayMessage(CalculatorAddon.prefix + "§c" + CalculatorAddon.getTranslation("calc.messages.error"));
             e.printStackTrace();
         }
         return true;
+    }
+
+    private Number solveProblem(String problem) {
+        return 69;
+    }
+
+    private String formatNumber(Number number) {
+        DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance(Locale.US);
+        DecimalFormatSymbols symbols = formatter.getDecimalFormatSymbols();
+        symbols.setGroupingSeparator('.');
+        symbols.setDecimalSeparator(',');
+        formatter.setDecimalFormatSymbols(symbols);
+        formatter.setMaximumFractionDigits(((float) number) % 1 == 0 ? 0 : 4);
+
+        return formatter.format(number);
     }
 }

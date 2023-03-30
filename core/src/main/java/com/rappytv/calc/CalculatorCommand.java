db91161b0bac969f215ec6719d5abfc840a6cc9f
RappyTV <rappytv@rappytv.com>
2023-03-30 22:30:48 +0200

    Added functionality (now fr)

--- core/src/main/java/com/rappytv/calc/CalculatorCommand.java
+++ core/src/main/java/com/rappytv/calc/CalculatorCommand.java
@@ -2,9 +2,6 @@ package com.rappytv.calc;
 
 import net.labymod.api.client.chat.command.Command;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
@@ -28,34 +25,116 @@ public class CalculatorCommand extends Command {
         }
         try {
             String problem = String.join(" ", arguments);
-            addon.displayMessage(CalculatorAddon.prefix + problem + " = " + formatNumber(solveProblem(problem)));
-        } catch (NumberFormatException | ScriptException e) {
+            addon.displayMessage(CalculatorAddon.prefix + problem + " = " + formatNumber(eval(problem)));
+        } catch (NumberFormatException e) {
             addon.displayMessage(CalculatorAddon.prefix + "§c" + CalculatorAddon.getTranslation("calc.messages.error"));
             e.printStackTrace();
         }
         return true;
     }
 
-    private Number solveProblem(String problem) throws ScriptException, NumberFormatException {
-        ScriptEngineManager manager = new ScriptEngineManager();
-        ScriptEngine engine = manager.getEngineByName("js");
-        Object num;
-
-        num = engine.eval(problem);
-
-        if(num instanceof Number) {
-            return (Number) num;
-        } throw new NumberFormatException();
-    }
-
-    private String formatNumber(Number number) {
+    private String formatNumber(double number) {
         DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance(Locale.US);
         DecimalFormatSymbols symbols = formatter.getDecimalFormatSymbols();
         symbols.setGroupingSeparator('.');
         symbols.setDecimalSeparator(',');
         formatter.setDecimalFormatSymbols(symbols);
-        formatter.setMaximumFractionDigits(((float) number) % 1 == 0 ? 0 : 4);
+        formatter.setMaximumFractionDigits(number % 1 == 0 ? 0 : 4);
 
         return formatter.format(number);
+    }
+
+    /**
+     *
+     * Function by <a href="https://stackoverflow.com/users/964243/boann">Boann</a> on <a href="https://stackoverflow.com/questions/3422673/how-to-evaluate-a-math-expression-given-in-string-form">StackOverflow</a>
+     */
+    public static double eval(final String str) throws NumberFormatException {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while(ch == ' ') nextChar();
+                if(ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if(pos < str.length()) throw new NumberFormatException();
+                return x;
+            }
+
+            double parseExpression() {
+                double x = parseTerm();
+                for(;;) {
+                    if(eat('+')) x += parseTerm(); // addition
+                    else if(eat('-')) x -= parseTerm(); // subtraction
+                    else return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for(;;) {
+                    if(eat('*')) x *= parseFactor(); // multiplication
+                    else if(eat('/')) x /= parseFactor(); // division
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+                if(eat('+')) return +parseFactor(); // unary plus
+                if(eat('-')) return -parseFactor(); // unary minus
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) { // parentheses
+                    x = parseExpression();
+                    if (!eat(')')) throw new NumberFormatException();
+                } else if((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+                    while((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(str.substring(startPos, this.pos));
+                } else if(ch >= 'a' && ch <= 'z') { // functions
+                    while(ch >= 'a' && ch <= 'z') nextChar();
+                    String func = str.substring(startPos, this.pos);
+                    if(eat('(')) {
+                        x = parseExpression();
+                        if(!eat(')')) throw new NumberFormatException();
+                    } else {
+                        x = parseFactor();
+                    }
+                    switch (func) {
+                        case "sqrt":
+                            x = Math.sqrt(x);
+                            break;
+                        case "sin":
+                            x = Math.sin(Math.toRadians(x));
+                            break;
+                        case "cos":
+                            x = Math.cos(Math.toRadians(x));
+                            break;
+                        case "tan":
+                            x = Math.tan(Math.toRadians(x));
+                            break;
+                        default:
+                            throw new NumberFormatException();
+                    }
+                } else {
+                    throw new NumberFormatException();
+                }
+
+                if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+
+                return x;
+            }
+        }.parse();
     }
 }
